5f0918f1fb73e76f2c15efb18808f80596e1be54
Saisantosh Chavakula <saisantoshchavakula6@hotmail.com>
2025-06-02 14:34:57 -0400

    Add Vite scripts to package.json and frontend

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="h-screen w-screen">
+      <div id="map" className="h-full w-full"></div>
+    </div>
+  );
+}
+
+export default App;
